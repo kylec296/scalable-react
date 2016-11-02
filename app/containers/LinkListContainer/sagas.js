6b8dfc74bb342a08a38f403217c987a709b68e28
Kyle Crouse <kylec296@gmail.com>
2016-11-02 15:10:00 -0500

    finished Adding Routes Module

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -1,19 +1,20 @@
 // import { take, call, put, select } from 'redux-saga/effects';
 import { call, put } from 'redux-saga/effects';
 import { takeLatest } from 'redux-saga';
-import { SELECT_TOPIC } from '../NavigationContainer/constants';
+// import { SELECT_TOPIC } from '../NavigationContainer/constants';
 import { requestLinksSucceeded, requestLinksFailed } from './actions';
+import { REQUEST_LINKS } from './constants';
 
-function fetchLinksFromServer(topic) {
+function fetchLinksFromServer(topicName) {
   //watch the quotes here should be slanted not straight up
-  return fetch(`http://localhost:3000/api/topics/${topic.name}/links`)
+  return fetch(`http://localhost:3000/api/topics/${topicName}/links`)
     .then(response => response.json());
 }
 
 //generator function to kick off stuff not a pure function
 function* fetchLinks(action) {
   try{
-    const links = yield call(fetchLinksFromServer, action.topic);
+    const links = yield call(fetchLinksFromServer, action.topicName);
     //dispatch action to store links
     yield put(requestLinksSucceeded(links));
 
@@ -25,9 +26,9 @@ function* fetchLinks(action) {
 
 // Individual exports for testing
 // * = generator function to kick off  async stuff
-export function* defaultSaga() {
-  yield* takeLatest(SELECT_TOPIC, fetchLinks);
-}
+ export function* defaultSaga() {
+   yield* takeLatest(REQUEST_LINKS, fetchLinks);
+ }
 
 // All sagas to be loaded
 export default [
--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,10 +1,12 @@
 // import { take, call, put, select } from 'redux-saga/effects';
-import { REQUEST_TOPICS } from './constants';
+import { REQUEST_TOPICS, SELECT_TOPIC, REQUEST_TOPICS_SUCCEEDED } from './constants';
 import { takeLatest } from 'redux-saga';
 // side effect to basically say go and call this function with these parameters
 //what are side effects
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import { requestTopicsSucceeded, requestTopicsFailed } from './actions';
+import { push } from 'react-router-redux';
+import selectNavigationContainer from './selectors';
 
 export function fetchTopicFromServer() {
   return fetch('http://localhost:3000/api/topics')
@@ -21,9 +23,27 @@ function* fetchTopics() {
   } catch (e) {
     yield put(requestTopicsFailed(e.message));
   }
-
 }
 
+function* selectDefaultTopic() {
+  const state = yield select(selectNavigationContainer());
+  if(!state.selectedTopic) {
+    yield put(push(`/topics/${state.topics[0].name}`));
+  }
+}
+
+export function* selectDefaultTopicSaga() {
+  yield* takeLatest(REQUEST_TOPICS_SUCCEEDED, selectDefaultTopic);
+}
+
+function* pushTopics(action) {
+ // put triggers an action and push to go and get the action
+  yield put(push(`/topics/${action.topic.name}`));
+}
+
+export function* selectTopicSaga() {
+  yield* takeLatest(SELECT_TOPIC, pushTopics);
+}
 // Individual exports for testing
 export function* fetchTopicSaga() {
   //basically saying wait around till you recieve this REQUEST_TOPICS action to get fired then call fetchTopics
@@ -34,4 +54,6 @@ export function* fetchTopicSaga() {
 // All sagas to be loaded
 export default [
   fetchTopicSaga,
+  selectTopicSaga,
+  selectDefaultTopicSaga,
 ];
